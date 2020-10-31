364fc44f881d2c2ed0ad0766ff25269275c03790
Yury Belousov <yury-belousov@yandex.ru>
2020-10-31 23:16:30 +0300

    combining frustum and ssd
    lidar&camera data

---
+++ 01-perception/02/eval_det.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+def voc_ap(rec, prec, use_07_metric=False):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
+    else:
+        # correct AP calculation
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+        # to calculate area under PR curve, look for points
+        # where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+
+        # and sum (\Delta recall) * prec
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def get_iou(box1, box2):
+    return box1.intersection(box2).area / box1.union(box2).area
+
+
+def eval_det_cls(pred, gt, ovthresh=0.25, use_07_metric=False):
+    """ Generic functions to compute precision/recall for object detection
+        for a single class.
+        Input:
+            pred: map of {img_id: [(bbox, score)]} where bbox is numpy array
+            gt: map of {img_id: [bbox]}
+            ovthresh: scalar, iou threshold
+            use_07_metric: bool, if True use VOC07 11 point method
+        Output:
+            rec: numpy array of length nd
+            prec: numpy array of length nd
+            ap: scalar, average precision
+    """
+
+    # construct gt objects
+    class_recs = {}  # {img_id: {'bbox': bbox list, 'det': matched list}}
+    npos = 0
+    for img_id in gt.keys():
+        bbox = np.array(gt[img_id])
+        det = [False] * len(bbox)
+        npos += len(bbox)
+        class_recs[img_id] = {'bbox': bbox, 'det': det}
+    # pad empty list to all other imgids
+    for img_id in pred.keys():
+        if img_id not in gt:
+            class_recs[img_id] = {'bbox': np.array([]), 'det': []}
+
+    # construct dets
+    image_ids = []
+    confidence = []
+    BB = []
+    for img_id in pred.keys():
+        for box, score in pred[img_id]:
+            image_ids.append(img_id)
+            confidence.append(score)
+            BB.append(box)
+    confidence = np.array(confidence)
+    BB = np.array(BB)  # (nd,4 or 8,3)
+
+    # sort by confidence
+    sorted_ind = np.argsort(-confidence)
+    sorted_scores = np.sort(-confidence)
+    BB = BB[sorted_ind, ...]
+    image_ids = [image_ids[x] for x in sorted_ind]
+
+    # go down dets and mark TPs and FPs
+    nd = len(image_ids)
+    tp = np.zeros(nd)
+    fp = np.zeros(nd)
+    for d in range(nd):
+        if d % 100 == 0: print(d)
+        R = class_recs[image_ids[d]]
+        bb = BB[d]
+        ovmax = -np.inf
+        BBGT = R['bbox']
+
+        if BBGT.size > 0:
+            # compute overlaps
+            for j in range(BBGT.shape[0]):
+                iou = get_iou(bb, BBGT[j])
+                if iou > ovmax:
+                    ovmax = iou
+                    jmax = j
+
+        #print d, ovmax
+        if ovmax > ovthresh:
+            if not R['det'][jmax]:
+                tp[d] = 1.
+                R['det'][jmax] = 1
+            else:
+                fp[d] = 1.
+        else:
+            fp[d] = 1.
+
+    # compute precision recall
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+    rec = tp / float(npos)
+    print('NPOS: ', npos)
+    # avoid divide by zero in case the first detection matches a difficult
+    # ground truth
+    prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
+    ap = voc_ap(rec, prec, use_07_metric)
+
+    return rec, prec, ap
